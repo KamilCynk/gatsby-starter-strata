7240738588ebc9e8ead2fa8743204db6423c6266
KamilCynk <kamilcynk@gmail.com>
2020-07-25 17:53:04 +0200

    v4

--- src/components/Gallery/constants/defaultImages.js
+++ src/components/Gallery/constants/defaultImages.js
@@ -4,12 +4,22 @@ import full03 from '../../../assets/images/fulls/03.jpg'
 import full04 from '../../../assets/images/fulls/04.jpg'
 import full05 from '../../../assets/images/fulls/05.jpg'
 import full06 from '../../../assets/images/fulls/06.jpg'
-import thumb02 from '../../../assets/images/thumbs/02.jpg'
+import full07 from '../../../assets/images/fulls/07.jpg'
+import full08 from '../../../assets/images/fulls/08.jpg'
+import full09 from '../../../assets/images/fulls/09.jpg'
+import full10 from '../../../assets/images/fulls/10.jpg'
+import full11 from '../../../assets/images/fulls/11.jpg'
 import thumb01 from '../../../assets/images/thumbs/01.jpg'
+import thumb02 from '../../../assets/images/thumbs/02.jpg'
 import thumb03 from '../../../assets/images/thumbs/03.jpg'
 import thumb04 from '../../../assets/images/thumbs/04.jpg'
 import thumb05 from '../../../assets/images/thumbs/05.jpg'
 import thumb06 from '../../../assets/images/thumbs/06.jpg'
+import thumb07 from '../../../assets/images/thumbs/07.jpg'
+import thumb08 from '../../../assets/images/thumbs/08.jpg'
+import thumb09 from '../../../assets/images/thumbs/09.jpg'
+import thumb10 from '../../../assets/images/thumbs/10.jpg'
+import thumb11 from '../../../assets/images/thumbs/11.jpg'
 
 
 export const DEFAULT_IMAGES = [
@@ -52,6 +62,41 @@ export const DEFAULT_IMAGES = [
       id: '6',
       source: full06,
       thumbnail: thumb06,
+      caption: '',
+      description: '',
+    },
+    {
+      id: '7',
+      source: full07,
+      thumbnail: thumb07,
+      caption: '',
+      description: '',
+    },
+    {
+      id: '8',
+      source: full08,
+      thumbnail: thumb08,
+      caption: '',
+      description: '',
+    },
+    {
+      id: '9',
+      source: full09,
+      thumbnail: thumb09,
+      caption: '',
+      description: '',
+    },
+    {
+      id: '10',
+      source: full10,
+      thumbnail: thumb10,
+      caption: '',
+      description: '',
+    },
+    {
+      id: '11',
+      source: full11,
+      thumbnail: thumb11,
       caption: '',
       description: '',
     }
